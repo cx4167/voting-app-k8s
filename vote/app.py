66c54dfc93b45d7032bf66a37852341e04e1eb6c
carbo-x <kdenish40@gmail.com>
2025-12-29 06:03:57 +0000

    adding thanks.html

--- vote/app.py
+++ vote/app.py
@@ -1,6 +1,6 @@
 import os
 import socket
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from redis import Redis
 
 app = Flask(__name__)
@@ -11,8 +11,12 @@ def vote():
     if request.method == 'POST':
         vote_data = request.form['vote']
         redis.rpush('votes', vote_data)
-        return jsonify({'message': 'Vote recorded successfully!'})
+        return redirect(url_for('thank_you'))
     return render_template('index.html', hostname=socket.gethostname())
+
+@app.route('/thanks')
+def thank_you():
+    return render_template('thanks.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
